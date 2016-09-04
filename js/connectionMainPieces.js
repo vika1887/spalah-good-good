efecc4657ab503dde6e3e6d09c9cc56ec4142146
korol <korolph@gmail.com>
2016-09-04 20:50:02 +0300

    create main skeleton

---
+++ js/connectionMainPieces.js
@@ -0,0 +1,4 @@
+$(function(){
+    $("header").load("parts/header.html");
+    $("footer").load("parts/footer.html");
+});
---
+++ js/scriptMainPage.js
@@ -0,0 +1,9 @@
+$( function() {
+    $(".j-testimonials").load("parts/testimonials.html", function() {
+
+        <!-- scripts for testimonials block connect here start -->
+
+        <!-- scripts for testimonials block connect here end -->
+
+    });
+} );
